f23386697c642d5b8dae8372d39c89566c413bfa
BTchorzewski <tchorzewskibartosz@gmail.com>
2021-12-02 16:06:05 +0100

    connect mongoDB

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 
-const { create, engine } = require('express-handlebars');
+const { engine } = require('express-handlebars');
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
 
 const { handlebarHelpers } = require('./views/handlebar-helpers');
-const { homeRouter } = require('./routers/home');
 
-const hbs = create({ extname: '.hbs', helpers: handlebarHelpers });
+const { homeRouter } = require('./routers/home');
 
 const app = express();
 
 app.engine('.hbs', engine({ extname: '.hbs', helpers: handlebarHelpers }));
 app.set('view engine', '.hbs');
+
 app.use(express.static('public'));
-app.use(homeRouter)
-app.listen(3000);
+
+// routers
+app.use(homeRouter);
+
+try {
+  app.listen(process.env.PORT || 3000, () => {
+    mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+    });
+
+    console.log(`Server starts on localhost:${process.env.PORT || 3000}`);
+
+  });
+} catch {
+  throw new Error('Something wrong with server');
+}
